c66fd176fc8ec26d26ca7dc56417c1a7d0351b23
alexssandro772 <alex.araujo77@gmail.com>
2023-08-22 11:29:38 -0300

    Alterando o nome da classe para ContaTerminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ContaTerminal{
+int numeroConta;
+String agencia;
+String nomeCliente;
+double saldo;
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Projeto Conta Bancária");
+
+        scanner.close();
+    }
+}
